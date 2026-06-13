9081552771198996acc8e9049a0ae1d3f6244459
RStanimirov <rumen.stanimirov@gmail.com>
2026-06-13 20:52:32 +0300

    Attempt fix for host

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,16 @@
 import * as api from './api.js';
 
-const host = 'https://devops-library-ci-cd-render.onrender.com'
-api.settings.host = 'https://devops-library-ci-cd-render.onrender.com';
+// const host = 'https://devops-library-ci-cd-render.onrender.com'
+// api.settings.host = 'https://devops-library-ci-cd-render.onrender.com';
+
+// const host = 'http://localhost:3030';
+// api.settings.host = 'http://localhost:3030';
+
+const host = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:3030'
+  : 'https://devops-library-ci-cd-render.onrender.com';
+
+api.settings.host = host;
 
 export const login = api.login;
 export const register = api.register;
